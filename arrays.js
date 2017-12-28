f0893d5966f3592330f212babd604be79b5e6c7a
Tyler Maxwell <tylermaxwell661@gmail.com>
2017-12-28 11:45:55 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,14 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  var newArray = [element, ...array];
+}
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  return array.unshift(element);
+}
+function addElementToEndOfArray(array, element) {
+  var newArray = [...array, element];
+}
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element);
+}
