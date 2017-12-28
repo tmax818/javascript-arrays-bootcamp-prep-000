549d4a32edf7491d97023c6bb1160dc26db6f101
Tyler Maxwell <tylermaxwell661@gmail.com>
2017-12-28 11:55:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,3 +21,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1);  
 }
+function destructivelyRemoveElementFromEndOfArray(array) {
+  array.pop();
+}
