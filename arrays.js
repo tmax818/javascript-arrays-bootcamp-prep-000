bbcbc8dcc121f0279f449f26119b5ae6d0c37c50
Tyler Maxwell <tylermaxwell661@gmail.com>
2017-12-28 12:19:48 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -19,7 +19,8 @@ function accessElementInArray(array, index) {
   return array[index];
 }
 function destructivelyRemoveElementFromBeginningOfArray(array) {
-  return array.shift();
+  array.shift();
+  return array;
 }
 function removeElementFromBeginningOfArray(array) {
   return array.slice(1);  
